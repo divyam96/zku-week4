e4426e295983e7c80259693fa7d2752fc9fa600c
Divyam Shah <divyam096@gmail.com>
2022-05-30 16:54:13 +0530

    Added Frontend and event listener for assignment 4

--- frontend_assignment/pages/index.tsx
+++ frontend_assignment/pages/index.tsx
@@ -1,17 +1,82 @@
 import detectEthereumProvider from "@metamask/detect-provider"
 import { Strategy, ZkIdentity } from "@zk-kit/identity"
 import { generateMerkleProof, Semaphore } from "@zk-kit/protocols"
-import { providers } from "ethers"
+import { Contract, providers, utils } from "ethers"
 import Head from "next/head"
 import React from "react"
+import { useState} from "react"
+import { useForm, Controller, SubmitHandler } from "react-hook-form"
+import Greeter from "artifacts/contracts/Greeters.sol/Greeters.json"
+import {
+  TextField,
+  Button,
+  ThemeProvider
+} from "@material-ui/core";
+import Box from '@mui/material/Box';
+import { createTheme } from '@material-ui/core/styles'
 import styles from "../styles/Home.module.css"
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+
+const theme = createTheme({
+  palette: {
+    type: "dark"
+  }
+});
+
+type FormValues = {
+  Name: string;
+  Age: number;
+  Address: string;
+};
+
+const defaultValues = {
+  Name: "",
+  Age: 18,
+  Address: ""
+};
+
+const schema = yup.object({
+  Name: yup.string().required(),
+  Age: yup.number().positive().integer(),
+  Address: yup.string()
+});
 
 export default function Home() {
     const [logs, setLogs] = React.useState("Connect your wallet and greet!")
+    const { handleSubmit,
+    register,
+    reset,
+    control,
+    formState: { errors } } = useForm<FormValues>({defaultValues});
+    const onSubmit: SubmitHandler<FormValues> = (data) => greet(JSON.stringify(data));
+    const [data, setData] = useState("");
+    const [greeting, setGreeting] = useState("")
 
-    async function greet() {
+    listenToGreets();
+
+
+    async function listenToGreets() {
+
+        // console.log("This is the start of greet listener")
+        const provider = new providers.JsonRpcProvider("http://localhost:8545")
+        const contract = new Contract("0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
+                                      Greeter.abi,
+                                      provider)
+
+        contract.on("NewGreeting", (greeting: string) => {
+            setGreeting("New Greeting Detected: " + utils.parseBytes32String(greeting));
+        })
+
+
+    }
+
+
+    async function greet(data) {
+
         setLogs("Creating your Semaphore identity...")
-
+        console.log(data)
+        schema.validate(data);
         const provider = (await detectEthereumProvider()) as any
 
         await provider.request({ method: "eth_requestAccounts" })
@@ -60,6 +125,7 @@ export default function Home() {
     }
 
     return (
+        <ThemeProvider theme={theme}>
         <div className={styles.container}>
             <Head>
                 <title>Greetings</title>
@@ -73,11 +139,47 @@ export default function Home() {
                 <p className={styles.description}>A simple Next.js/Hardhat privacy application with Semaphore.</p>
 
                 <div className={styles.logs}>{logs}</div>
-
-                <div onClick={() => greet()} className={styles.button}>
-                    Greet
-                </div>
+                <form onSubmit={handleSubmit(onSubmit)} className="form">
+                  <section>
+                     <Controller
+                       render={({ field }) => <TextField {...field} label="Name"/>}
+                       name="Name"
+                       control={control}
+                     />
+                 </section>
+                 <section>
+                  <Controller
+                    render={({ field }) => <TextField {...field} label="Age" type="number"/>}
+                    name="Age"
+                    control={control}
+                  />
+                </section>
+                <section>
+                 <Controller
+                   render={({ field }) => <TextField {...field} label="Address" multiline minRows={4}/>}
+                   name="Address"
+                   control={control}
+                 />
+               </section>
+               <Box sx={{p: 1, m: 5}}>
+                    <Button variant="contained" type="submit">Submit</Button>
+              </Box>
+                 </form>
+                 <Box
+                     sx={{
+                       width: 300,
+                       height: 60,
+                       backgroundColor: 'primary.dark',
+                       '&:hover': {
+                         backgroundColor: 'primary.main',
+                         opacity: [0.9, 0.8, 0.7],
+                       },
+                     }}
+                >
+                 <p align="center">{greeting}</p>
+                </Box>
             </main>
         </div>
+        </ThemeProvider>
     )
 }
